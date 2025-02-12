4c8bdc4eacbca9380f11558eba5273c639a9b3f7
Rabi prajapati <cvile.last8@gmail.com>
2025-02-12 09:52:27 -0600

    Add files via upload

---
+++ dump.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+file=$1
+filename=${file%.*}
+$(mkdir $filename)
+
+#getting streams
+streams=$(oledump.py $file | grep -i ': m' | cut -d: -f1)
+for stream in $streams; do
+	oledump.py $file -s $stream -v > ${filename}/s$stream
+done
+
+#getting objects
+objects=$(oledump.py $file | grep -i ': o' | cut -d: -f1)
+for object in $objects; do
+	oledump.py $file -s $object -v > ${filename}/o$object
+done
+
+echo "'$file' extracted successfully"
